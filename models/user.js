fc3bdb93e56989d41efd8ef82290130eefa871a3
Hetal Rajgor <hetalrajgorb@gmail.com>
2022-04-21 12:37:35 +0530

    Modifed userModel to handle forgot password scenario

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
+const jwtToken = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -34,13 +37,48 @@ const userSchema = new mongoose.Schema({
     default: "user",
   },
   forgotPasswordToken: String,
-  forgotPasswordExpiry: {
-    type: Date,
-  },
+  forgotPasswordExpiry: Date,
   createdAt: {
     type: Date,
     default: Date.now,
   },
 });
+
+//encrypting the password before saving -HOOKS
+userSchema.pre("save", async function (next) {
+  // Only run this function if password was moddified (not on other update functions)
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+});
+
+//validate password with password sent by user
+userSchema.methods.isValidatedPassword = async function (userSentPassword) {
+  return await bcrypt.compare(userSentPassword, this.password);
+};
+
+//creating and returning Jwt token
+userSchema.methods.getJWTToken = function () {
+  return jwtToken.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRY,
+  });
+};
+
+//forgot passowrd token
+userSchema.methods.getForgotPasswordToken = function () {
+  //generate a long & random string
+  const forgotToken = crypto.randomBytes(20).toString("hex");
+
+  //optional but better to do
+  //getting a hash - make sure to get a hash on backend
+  this.forgotPasswordToken = crypto
+    .createHash("sha256")
+    .update(forgotToken)
+    .digest("hex");
+
+  //time of token
+  this.forgotPasswordExpiry = Date.now() + 20 * 60 * 1000;
+
+  return forgotToken;
+};
 
 module.exports = mongoose.model("User", userSchema);
